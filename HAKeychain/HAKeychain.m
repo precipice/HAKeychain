5863bf0c7b180f4a3458c0a5ea8a70bed1b4f579
Marc Hedlund <marc@precipice.org>
2011-07-25 00:56:15 -0400

    README updates and code arrangement.

--- HAKeychain/HAKeychain.m
+++ HAKeychain/HAKeychain.m
@@ -16,6 +16,40 @@
 
 @implementation HAKeychain
 
++ (BOOL)createPassword:(NSString *)password
+            forService:(NSString *)service
+               account:(NSString *)account
+              keychain:(SecKeychainRef)keychain
+                 error:(NSError **)error {
+    
+    if (password == NULL || ![HAKeychain validService:service account:account]) {
+        [HAKeychain setCode:errSecParam error:error];
+        return NO;
+    }
+
+    const char *passwordUTF8 = [password UTF8String];
+    const char *serviceUTF8  = [service UTF8String];
+    const char *accountUTF8  = [account UTF8String];
+    SecKeychainItemRef item = nil;
+
+    OSStatus status = SecKeychainAddGenericPassword(keychain,
+                                                    (UInt32)strlen(serviceUTF8),
+                                                    serviceUTF8,
+                                                    (UInt32)strlen(accountUTF8),
+                                                    accountUTF8,
+                                                    (UInt32)strlen(passwordUTF8),
+                                                    passwordUTF8,
+                                                    &item);
+    if (item) CFRelease(item);
+
+    if (status != noErr) {
+        [HAKeychain setCode:status error:error];
+    }
+
+    return status == noErr;
+}
+
+
 + (BOOL)validService:(NSString *)service account:(NSString *)account {
     if (service == NULL || [service length] == 0 ||
         account == NULL || [account length] == 0) {
@@ -181,7 +215,7 @@
                 description = @"General keychain error.";
                 break;
         }
-    
+        
         NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys:
                                   description, NSLocalizedDescriptionKey, nil];
         
@@ -190,40 +224,5 @@
                                  userInfo:userInfo];
     }    
 }
-
-
-+ (BOOL)createPassword:(NSString *)password
-            forService:(NSString *)service
-               account:(NSString *)account
-              keychain:(SecKeychainRef)keychain
-                 error:(NSError **)error {
-    
-    if (password == NULL || ![HAKeychain validService:service account:account]) {
-        [HAKeychain setCode:errSecParam error:error];
-        return NO;
-    }
-
-    const char *passwordUTF8 = [password UTF8String];
-    const char *serviceUTF8  = [service UTF8String];
-    const char *accountUTF8  = [account UTF8String];
-    SecKeychainItemRef item = nil;
-
-    OSStatus status = SecKeychainAddGenericPassword(keychain,
-                                                    (UInt32)strlen(serviceUTF8),
-                                                    serviceUTF8,
-                                                    (UInt32)strlen(accountUTF8),
-                                                    accountUTF8,
-                                                    (UInt32)strlen(passwordUTF8),
-                                                    passwordUTF8,
-                                                    &item);
-    if (item) CFRelease(item);
-
-    if (status != noErr) {
-        [HAKeychain setCode:status error:error];
-    }
-
-    return status == noErr;
-}
-
 
 @end
--- README.md
+++ README.md
@@ -4,14 +4,36 @@
 This isn't ready to be used yet. The rest of this README is a draft.
 
 
+## Features ##
+
+* Easier API for dealing with the OS X Keychain.
+* Works in XCode 4.0.
+* Provides easy, localizable error reporting.
+
+## Creating a Password ##
+
+For almost every case, saving passwords in the user's default (login) keychain
+makes the most sense. (You cannot create a password for the same service and
+account twice; if you want to update an already-saved password, use the update
+method below instead.) Here's how to create a new password in the default
+keychain:
+
+    NSError *error = nil;
+    BOOL success = [HAKeychain createPassword:@"testpass"
+                                   forService:@"testservice"
+                                      account:@"testaccount"
+                                     keychain:NULL
+                                        error:&error];
+
 ## Reporting Errors ##
 
 HAKeychain supports localized descriptions of Keychain-related errors, so
 you can very easily report problems to users. These messages are lightly
-modified from the error descriptions provided by Apple.
+modified from the error descriptions provided by Apple (in comments on 
+<Security/SecBase.h>).
 
-After any call, if an error is reported (if a BOOL return is NO or an object
-return is nil), you can show a localized error dialog as follows:
+After any call, if an error is reported (if a BOOL return is `NO` or an object
+return is `nil`), you can show a localized error dialog as follows:
 
     if (success == NO && error != nil) {  // or: result == nil && error != nil
         NSAlert *alert = [NSAlert alertWithError:error];
@@ -20,4 +42,25 @@ return is nil), you can show a localized error dialog as follows:
 
 Hopefully the error message is helpful to the user. If not, you can always
 override the message provided.  Localized strings are only provided in English,
-though pull requests for other languages are welcome.
+though pull requests for other languages are welcome (see
+[Localizable.strings](https://github.com/precipice/HAKeychain/blob/master/HAKeychain/en.lproj/Localizable.strings)
+if you want to contribute).
+
+## Example ##
+
+    // Save a new password the user has entered.
+    NSError *error = nil;
+    BOOL success = [HAKeychain createPassword:@"newpassword"
+                                   forService:@"myservice"
+                                      account:@"myaccount"
+                                     keychain:NULL
+                                        error:&error];
+    
+    if (success == NO && error != nil) {
+        NSAlert *alert = [NSAlert alertWithError:error];
+        [alert runModal];
+    } else {
+        // ... success ...
+    }
+
+- Marc Hedlund, <marc@precipice.org>
